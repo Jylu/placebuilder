7614b692801e7b129e6c26a91908a08997ee3d99
Ye Shen <aodioicq@gmail.com>
2012-06-20 16:27:55 -0400

    edit migration file

--- db/migrate/20120619201512_add_phone_and_organization_and_position.rb
+++ db/migrate/20120619201512_add_phone_and_organization_and_position.rb
@@ -1,7 +1,6 @@
-class AddPhoneAndOrganizationAndSectorTagAndTypeTagToResident < ActiveRecord::Migration
+class AddPhoneAndOrganization < ActiveRecord::Migration
   def change
     add_column :residents, :phone, :integer
     add_column :residents, :organization, :string
-    add_column :residents, :position, :string
   end
 end
