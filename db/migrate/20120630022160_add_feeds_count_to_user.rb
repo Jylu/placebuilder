c5963b27ee6f3314c37943a13f35d6fa3ebf52f8
Jeffrey Lu <Ziruga@gmail.com>
2012-07-02 16:49:11 -0400

    Made some migration files less error-prone to migrate with

--- db/migrate/20120630022160_add_feeds_count_to_user.rb
+++ db/migrate/20120630022160_add_feeds_count_to_user.rb
@@ -1,8 +1,10 @@
 class AddFeedsCountToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :feeds_count, :integer, null:false, :default => 0
+
+    User.reset_column_information
     User.find_each do |user|
-      user.update_attribute(:feeds_count, user.managable_feeds.length)
+      User.update_counters user.id, :feeds_count => user.managable_feeds.length
       user.save
     end
     
