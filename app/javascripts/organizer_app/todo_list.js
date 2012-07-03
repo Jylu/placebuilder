d991cae31dc73b0594350d2b3ce362e7e3ff99bd
Jeffrey Lu <Ziruga@gmail.com>
2012-07-03 16:22:59 -0400

    Added add tag button to task viewer(doesn't work yet)

--- app/javascripts/organizer_app/todo_list.js
+++ app/javascripts/organizer_app/todo_list.js
@@ -4,22 +4,27 @@ OrganizerApp.TodoList = CommonPlace.View.extend({
   template: "organizer_app.todo-list",
 
   events: {
+    "click #add-tag" : "addTag",
   },
 
   initialize: function() {
     models = this.collection.models;
+    community = this.options.community;
+  },
+
+  addTag: function() {
   },
 
   profiles: function() {
    // alert(this.$().attr('name'));
     var todos = possTodos;
-    var tagged = _.filter(models, _.bind(function(model) {
+    var tagged = _.filter(models, function(model) {
       return true;
-    }, this));
+    });
 
-    var names = _.map(tagged, _.bind(function(model) {
+    var names = _.map(tagged, function(model) {
       return model.full_name();
-    }, this));
+    });
     
     return names;
   },
@@ -27,5 +32,7 @@ OrganizerApp.TodoList = CommonPlace.View.extend({
   todos: function() {
     possTodos = this.options.community.get('resident_todos');
     return this.options.community.get('resident_todos');
-  }
+  },
+
+  tags: function() { return community.get('resident_tags'); },
 });
