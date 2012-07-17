74f4129c144a3c302d19daadf35e0b6adb86a6ff
Jeffrey Lu <Ziruga@gmail.com>
2012-07-17 19:56:42 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120711191214) do
+ActiveRecord::Schema.define(:version => 20120716223018) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -417,6 +417,7 @@ ActiveRecord::Schema.define(:version => 20120711191214) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "community_id"
+    t.string   "carrier_route"
   end
 
   create_table "subscriptions", :force => true do |t|
