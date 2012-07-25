4ba9711c36e11ee6f93539382d004173e1b04ceb
Ye Shen <aodioicq@gmail.com>
2012-07-25 16:15:57 -0400

    fix migration

--- db/migrate/20120619201512_add_phone_and_organization.rb
+++ db/migrate/20120619201512_add_phone_and_organization.rb
@@ -1,6 +1,10 @@
 class AddPhoneAndOrganization < ActiveRecord::Migration
-  def change
+  def up
     add_column :residents, :phone, :integer
     add_column :residents, :organization, :string
+  end
+  def down
+    remove_column :residents, :phone, :integer
+    remove_column :residents, :organization, :string
   end
 end
--- db/migrate/20120619224822_add_new_tags_and_notes_to_resident.rb
+++ db/migrate/20120619224822_add_new_tags_and_notes_to_resident.rb
@@ -1,7 +1,12 @@
 class AddNewTagsAndNotesToResident < ActiveRecord::Migration
-  def change
+  def up
     add_column :residents, :sector_tags, :text
     add_column :residents, :type_tags, :text
     add_column :residents, :notes, :string
+  end
+  def down
+    remove_column :residents, :sector_tags, :text
+    remove_column :residents, :type_tags, :text
+    remove_column :residents, :notes, :string
   end
 end
