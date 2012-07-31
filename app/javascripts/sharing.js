349c1b12d342e9aa3e4ec3a18ef489ae60b60949
Jason Berlinsky <jberlinsky@gmail.com>
2012-07-31 19:13:46 -0400

    Fix indentation

--- app/javascripts/sharing.js
+++ app/javascripts/sharing.js
@@ -2,17 +2,17 @@ function share(url, name1, description, name, community) {
   var header = name + " Posted an Event to " + community + "'s CommonPlace";
   FB.login(
     function(response) {
-      if (response.session) {
-        FB.ui({
-          method: 'feed',
-          name: header,
-          link: url,
-	  picture: '',
-	  caption: community + ' CommonPlace',
-	  description: name + ": " + description,
-	  message: 'Message'
-        },
-              function(response) {});
-      }
-    });
+    if (response.session) {
+      FB.ui({
+        method: 'feed',
+        name: header,
+        link: url,
+        picture: '',
+        caption: community + ' CommonPlace',
+        description: name + ": " + description,
+        message: 'Message'
+      },
+      function(response) {});
+    }
+  });
 }
