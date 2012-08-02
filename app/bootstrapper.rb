24d2df2c3811626d2234a8f550ba2b642db708cb
Bryan Kerrigan <bryankerrigan@gmail.com>
2012-08-02 15:24:58 -0700

    Initial public post url commit. Public urls work to display individual posts/events/ect.

--- app/bootstrapper.rb
+++ app/bootstrapper.rb
@@ -88,7 +88,14 @@ class Bootstrapper < Sinatra::Base
     erb :art_project
   end
 
-  get %r{([\w]+)/register.*} do
+  get %r{([\w]+)/show.*} do 
+    set_account
+    @community = Community.find_by_slug(params[:captures].first)
+
+    erb @account ? :application : :show
+  end
+
+  get %r{([\w]+)/register.*} do 
     @community = Community.find_by_slug(params[:captures].first)
     erb :register
   end
