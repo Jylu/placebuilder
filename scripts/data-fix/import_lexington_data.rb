fb45df290b545f49c4c1544f89d0d84af14de709
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-11 01:47:14 -0400

    Wrap Lexington data import in debugging text and begin/rescue

--- scripts/data-fix/import_lexington_data.rb
+++ scripts/data-fix/import_lexington_data.rb
@@ -2,7 +2,12 @@ class ImportLexingtonData < ActiveRecord::Base
 
   community = Community.find_by_name("Lexington")
     s = Rails.root.join("scripts", "data-fix", "LexMelissadata.csv")
+    i = 0
     CSV.foreach(s, :headers => true) do |row|
+      i += 1
+      if i % 500 == 0
+        puts "Record ##{i}"
+      end
       h = row.to_hash
 
       first = h["FirstName"].nil? ? nil : h["FirstName"].capitalize.strip
@@ -13,10 +18,14 @@ class ImportLexingtonData < ActiveRecord::Base
       street = h["ADDRESS"].squeeze(" ").strip
       zip = h["Zip"].to_i
 
-      StreetAddress.create(address: street,
+      begin
+        StreetAddress.create(address: street,
                            carrier_route: h["Crrt"],
                            zip_code: zip,
                            unreliable_name: name,
                            community: community)
+      rescue
+        puts "Could not import line #{i}"
+      end
     end
 end
