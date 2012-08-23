91f2ee1d0e394ac6bbcbf325177d99bd563800bc
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-23 00:38:19 -0400

    Geolocation

---
+++ script/data-fix/add_zip_codes_to_communities.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+ZIP_CODES = {
+  "Warwick" => "10990",
+  "Fayetteville" => "72701",
+  "Raleigh" => "27601",
+  "StPaul" => "55112",
+  "Farragut" => "37934",
+  "Marquette" => "49855",
+  "Concord" => "01742",
+  "Lexington" => "02421",
+  "Cambridge" => "02141",
+  "HarvardNeighbors" => "02138"
+}
+
+ZIP_CODES.each do |slug, zip_code|
+  begin
+    Community.where(slug: slug).first.update_attributes(zip_code: zip_code)
+  rescue
+    puts "Could not find community '#{slug}'"
+  end
+end
