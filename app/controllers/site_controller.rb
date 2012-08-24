0d41c08b8a77485b323168509aa3364ca80f2b5d
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-24 10:17:56 -0400

    Server-side geolocation

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,4 +5,24 @@ class SiteController < ApplicationController
   def privacy ; end
 
   def terms ; end
+
+  def info
+    unless params[:locate] == "false"
+      # Get user's location from IP Address
+      # Send them to the right community's about page
+      closest_community = Community.all.map do |community|
+        {
+          distance: CoordinateDistance.distance(
+              request.location,
+              {
+                latitude: community.latitude,
+                longitude: community.longitude
+              }),
+          slug: community.slug
+        }
+      end.sort_by { |h| h[:distance] }.first
+      redirect_to "/#{closest_community[:slug]}/about"
+    end
+    render layout: nil
+  end
 end
---
+++ lib/coordinate_distance.rb
@@ -0,0 +1,20 @@
+module CoordinateDistance
+  R = 6371
+
+  def self.to_radian(number)
+    number * Math::PI / 180
+  end
+
+  def self.distance(source, destination)
+    delta_latitude = to_radian(destination.latitude - source.longitude)
+    delta_longitude = to_radian(destination.longitude - source.longitude)
+    source_latitude = to_radian(source.latitude)
+    destination_latitude = to_radian(source.latitude)
+
+    a = Math.sin(delta_latitude / 2) * Math.sin(delta_latitude / 2) +
+      Math.sin(delta_longitude / 2) * Math.sin(delta_longitude / 2) * Math.cos(source_latitude) *
+      Math.cos(destination_latitude)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    R * c
+  end
+end
