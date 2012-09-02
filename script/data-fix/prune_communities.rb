713f401ac5c37f1c442603fbb1743ac3fde74e8f
Jason Berlinsky <jberlinsky@gmail.com>
2012-09-02 11:38:00 -0400

    Prune more communities

--- script/data-fix/prune_communities.rb
+++ script/data-fix/prune_communities.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/env ruby
 
-COMMUNITY_SLUGS = [
-  "Kincardine",
-  "upperuws"
+COMMUNITY_SLUGS = %w[
+  Kincardine
+  upperuws
+  Hinkely
+  GreenwoodLake
+  sega
+  StPaul
+  Farragut
 ]
 
 COMMUNITY_SLUGS.each do |slug|
-  community = Community.where(slug: slug).first
-  community.should_delete = treu
-  community.destroy
+  begin
+    community = Community.where(slug: slug).first
+    community.should_delete = true
+    community.destroy
+  rescue; end
 end
