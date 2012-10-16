4d310bc60cfae88afe0e36767cb683005d99db5f
Jeffrey Lu <Ziruga@gmail.com>
2012-10-16 17:35:49 -0400

    Warning model for flagging posts

---
+++ app/models/warning.rb
@@ -0,0 +1,11 @@
+class Warning < ActiveRecord::Base
+  include Trackable
+  after_create :track_posted_content
+
+  belongs_to :user
+  belongs_to :warnable, :polymorphic => true
+
+  def warned_at
+    self.updated_at
+  end
+end
