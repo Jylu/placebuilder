431d2fbe2158a3a8a6a192f8e82a9bbadaebef84
Jason Berlinsky <jason@barefootcoders.com>
2013-08-24 20:16:05 -0400

    Improve development e-mail system

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,7 @@ Commonplace::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :letter_opener
 end
 
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,15 +1,19 @@
-$MailDeliveryMethod = ENV['mail_delivery_method'].try(:intern) || :file
+require "letter_opener"
 
-$MailDeliveryOptions = 
-  if $MailDeliveryMethod == :file
+$MailDeliveryMethod = ENV['mail_delivery_method'].try(:intern) || LetterOpener::DeliveryMethod
+
+$MailDeliveryOptions =
+  if $MailDeliveryMethod == LetterOpener::DeliveryMethod
+    # {:location => Rails.root.join('tmp','inboxes')}
+    puts "E-mails are being delivered to tmp/inboxes!"
     {:location => Rails.root.join('tmp','inboxes')}
   else
-    { 
+    {
     :address => ENV['mail_address'],
     :port => ENV['mail_port'],
     :domain => ENV['domain'],
     :authentication => ENV['mail_authentication'],
     :user_name => ENV['mail_username'],
-    :password => ENV['mail_password'] 
-    } 
+    :password => ENV['mail_password']
+    }
   end
